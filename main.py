e2ea901a9270a75fd1f638b27d4638a8f1bbd672
Milansuman <milansuman2016@gmail.com>
2024-02-28 01:40:49 +0530

    add ability to send message

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 from models import text_to_text
 import os
-
 from dotenv import load_dotenv
+import whatsapp
 
 load_dotenv()
 TOGETHER_API_KEY = os.environ["TOGETHER_API_KEY"]
-
 
 if __name__ == "__main__":
     text_data = text_to_text.text_to_text(
         "Tell me about San Francisco", TOGETHER_API_KEY
     )
     print(text_data)
+    whatsapp.sendMessage(text_data, "+918921964884")
