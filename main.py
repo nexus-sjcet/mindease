3d81c0642032f4a6f24fe2d4c59d580c2dd5cbee
Milansuman <milansuman2016@gmail.com>
2024-02-28 02:32:32 +0530

    sync with main

--- main.py
+++ main.py
@@ -2,18 +2,12 @@ import os
 from fastapi import FastAPI
 from routes import chat
 from models import text_to_text
+import whatsapp
 
 app = FastAPI()
 app.include_router(chat.app, prefix="/api/chat", tags=["chat"])
-import os
-from dotenv import load_dotenv
-import whatsapp
-
-load_dotenv()
-TOGETHER_API_KEY = os.environ["TOGETHER_API_KEY"]
 
 if __name__ == "__main__":
-
     text_data = text_to_text.text_to_text("Tell me about San Francisco")
     print(text_data)
     whatsapp.sendMessage(text_data, "+918921964884")
