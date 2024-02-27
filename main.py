8a1e043bcd41d2ca1e91df19abacfc4537bd1e36
reenphygeorge <reenphygeorge@gmail.com>
2024-02-28 00:28:54 +0530

    chore: project init

---
+++ main.py
@@ -0,0 +1,5 @@
+from models import text_to_text
+
+if __name__ == "__main__":
+    text_data = text_to_text.text_to_text("Tell me about San Francisco")
+    print(text_data)
---
+++ models/text_to_text.py
@@ -0,0 +1,28 @@
+from openai import OpenAI
+import os
+
+TOGETHER_API_KEY = os.environ.get("TOGETHER_API_KEY")
+
+
+def text_to_text(message):
+
+    client = OpenAI(
+        api_key=TOGETHER_API_KEY,
+        base_url="https://api.together.xyz",
+    )
+
+    chat_completion = client.chat.completions.create(
+        messages=[
+            {
+                "role": "system",
+                "content": "You are an AI assistant",
+            },
+            {
+                "role": "user",
+                "content": message,
+            },
+        ],
+        model="mistralai/Mistral-7B-Instruct-v0.1",
+        max_tokens=1024,
+    )
+    return chat_completion.choices[0].message.content
