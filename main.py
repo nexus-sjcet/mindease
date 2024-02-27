5ae9bd2641a0f86c296d48a4057c316c242d9847
Milansuman <milansuman2016@gmail.com>
2024-02-28 02:36:32 +0530

    fix requirements

--- main.py
+++ main.py
@@ -6,8 +6,3 @@ import whatsapp
 
 app = FastAPI()
 app.include_router(chat.app, prefix="/api/chat", tags=["chat"])
-
-if __name__ == "__main__":
-    text_data = text_to_text.text_to_text("Tell me about San Francisco")
-    print(text_data)
-    whatsapp.sendMessage(text_data, "+918921964884")
