852bd13565688c76408c56eba90081ddfe93ab19
Milansuman <milansuman2016@gmail.com>
2024-02-28 11:28:13 +0530

    add ability to save graphs

--- main.py
+++ main.py
@@ -3,8 +3,11 @@ from fastapi import FastAPI
 from routes import chat, score
 from models import text_to_text
 import whatsapp
+import graph
 
 app = FastAPI()
 app.include_router(chat.app, prefix="/api/chat", tags=["chat"])
 app.include_router(score.app, prefix="/api/score", tags=["score"])
 
+
+graph.generate_graph(3,5,2,0.4,4)
